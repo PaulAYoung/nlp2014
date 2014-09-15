b4fd6df4e85495ea550793fcd0efe11592945eaa
Paul Young <paulandresyoung@gmail.com>
2014-09-15 10:18:42 -0700

    simple corpus loader

---
+++ corpii.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This module is here to make it easy load various text corpuses into other
+scripts.
+"""
+
+from os import path
+
+from nltk.corpus.reader import PlaintextCorpusReader
+
+pwd = path.curdir
+
+
+def load_pres_debates():
+    """
+    Returns the corpus for the presidential debates.
+    """
+    debates = PlaintextCorpusReader(path.join(pwd, "pres_debates"), "./*.txt")
+    return debates
